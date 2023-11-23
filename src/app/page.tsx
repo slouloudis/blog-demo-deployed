a89a241aeefac2057da1bb763d138e5780fecf72
Sam Louloudis <samanthaleelouloudis@gmail.com>
2023-11-23 09:37:32 +0000

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>This is the home page. The root route. </h1>
+       <h1>This is a change so we can see how things work! </h1>
     </main>
   )
 }
